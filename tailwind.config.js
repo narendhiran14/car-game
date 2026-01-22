04bc18c70193f488726f8e1e4f5afdbbd7eb52a8
narendhiran14 <2005m.narendhiran@gmail.com>
2026-01-22 10:33:49 +0530

    feat: Update project dependencies and configuration
    Updates project dependencies to latest versions and configures Vite, TypeScript, Tailwind CSS, and PostCSS for development. This includes adjusting import maps, type definitions, and build scripts to align with newer package versions and best practices.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        orbitron: ['Orbitron', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
